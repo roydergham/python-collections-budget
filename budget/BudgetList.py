6d54aca3ca96a642f4783266f65e806b494088ce
roydergham <roy.dergham@gmail.com>
2022-05-05 14:15:07 +0300

    1

--- budget/BudgetList.py
+++
@@ -1,9 +0,0 @@
-
-class BudgetList():
-    def __init__(self, budget):
-        self.budget = budget
-        self.sum_expenses = 0
-        self.expenses = []
-        self.sum_overages = 0
-        self.overages = []
-
